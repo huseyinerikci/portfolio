1c9f95284247b1f287219a0f589f6143bbad7ffb
Hüseyin <hsyn51671@gmail.com>
2025-03-11 10:52:51 +0300

    update analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import { ThemeProvider } from "next-themes";
 import NextNProgress from "nextjs-progressbar";
 import { cyan } from "tailwindcss/colors";
+import { Analytics } from "@vercel/analytics/react";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
@@ -8,6 +9,7 @@ export default function App({ Component, pageProps }) {
     <ThemeProvider attribute="class">
       <NextNProgress color={cyan[500]} height={4} />
       <Component {...pageProps} />
+      <Analytics />
     </ThemeProvider>
   );
 }
